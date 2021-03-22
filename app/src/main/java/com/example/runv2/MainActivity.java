97a523fa265e8a52828dffa0c145368b2cee5513
Samwalker974 <zerzamo@gmail.com>
2021-03-22 19:53:41 +0100

    chef op=46

--- app/src/main/java/com/example/runv2/MainActivity.java
+++ app/src/main/java/com/example/runv2/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         int op=45;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        op=54;
     }
 }
